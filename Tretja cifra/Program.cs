c13c980e0de8c47087c2d3776eff8933f7110e57
Martin <rauch83@yandex.ru>
2022-08-14 14:11:08 +0300

    tretja cifra

---
+++ Tretja cifra/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("введите число из трех цифр");
+int a = Convert.ToInt32(Console.ReadLine());
+string str = a.ToString();
+int[] b = new int[str.Length];
+for( int i=0; i< str.Length; i++)
+{
+    b[i] = int.Parse(str[i].ToString());
+}
+
+void PrintArray(int[] b)
+{
+     int count = b.Length; 
+     int position = 0;
+     while(position < count)
+     {
+         Console.WriteLine(b[2]);
+         break;
+     }
+}
+
+if (b.Length == 3)
+{
+    Console.Write("Третья цифра числа - ");
+    PrintArray(b);
+    Console.WriteLine();
+}
+if (b.Length > 3)
+{
+    Console.Write("Ошибка! Вы ввели больше трех цифр.");
+}
+if (b.Length < 3)
+{
+    Console.Write("Третьей цифры нет!");
+}

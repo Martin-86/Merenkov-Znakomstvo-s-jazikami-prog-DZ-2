adcc1c0d2df34bc2e7a45a230ce0b0b7ab0958f8
Martin <rauch83@yandex.ru>
2022-08-14 17:15:32 +0300

    den nedeli adde

---
+++ Den nedeli/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ( a <= 7 && a >= 1)
+{
+    if (a == 6 || a == 7)
+    {
+        Console.WriteLine("Выходной день.");
+    }
+    else
+    {
+        Console.WriteLine("Будний день.");
+    }
+}
+else 
+{
+    Console.WriteLine("Нет такого дня.");
+}
+
+
+
+

4fde5246d93ec45bd05ecd1413d1d9390cc1ccdb
Martin <rauch83@yandex.ru>
2022-08-15 02:28:54 +0300

    questions added

--- Vtoraja cifra/Program.cs
+++ Vtoraja cifra/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("введите число из трех цифр");
 int a = Convert.ToInt32(Console.ReadLine());
-string str = a.ToString();
+string str = a.ToString();           // Скажите, пожалуйста, почему ToString() используется 2 раза? 1 раз здесь.
 int[] b = new int[str.Length];
 for( int i=0; i< str.Length; i++)
 {
-    b[i] = int.Parse(str[i].ToString());
-}
+    b[i] = int.Parse(str[i].ToString()); // Второй раз здесь. str[i].ToString() объект из string опять преобразовавают в string?
+} // И объясните пожалуйста словами, что происходит в строке 7?
 
 void PrintArray(int[] b)
 {
